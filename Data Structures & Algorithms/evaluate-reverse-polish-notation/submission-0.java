3b16abbc0551774182927038efd6d2a454dc7e1e
Attiq B <137288961+A99TI@users.noreply.github.com>
2026-04-24 09:02:35 +0100

    Update: evaluate-reverse-polish-notation - submission-0

--- Data Structures & Algorithms/evaluate-reverse-polish-notation/submission-0.java
+++ Data Structures & Algorithms/evaluate-reverse-polish-notation/submission-0.java
@@ -1,39 +1,31 @@
 class Solution {
     public int evalRPN(String[] tokens) {
-        
         Deque<Integer> stack = new ArrayDeque<>();
-        
 
-        for(String token: tokens){
-            if (token.equals("+")){
-                int num1 = stack.pop();
-                int num2 = stack.pop();
-                int total = num1 + num2;
-                stack.push(total);
-
-            } else if (token.equals("-")){
-                int num1 = stack.pop();
-                int num2 = stack.pop();
-                int total = num2 - num1;
-                stack.push(total);
-
-            } else if (token.equals("*")){
-                int num1 = stack.pop();
-                int num2 = stack.pop();
-                int total = num1 * num2;
-                stack.push(total);
-
-            } else if (token.equals("/")){
-                int num1 = stack.pop();
-                int num2 = stack.pop();
-                int total = num2 / num1;
-                stack.push(total);
-            } else {
+        for (String token : tokens) {
+            if (token.matches("-?\\d+")) {
                 int num = Integer.parseInt(token);
                 stack.push(num);
+            } else {
+                int num1 = stack.pop();
+                int num2 = stack.pop();
+
+                int total = 0;
+
+                if (token.equals("+")) {
+                    total = num2 + num1;
+                } else if (token.equals("-")) {
+                    total = num2 - num1;
+                } else if (token.equals("*")) {
+                    total = num2 * num1;
+                } else if (token.equals("/")) {
+                    total = num2 / num1;
+                }
+
+                stack.push(total);
             }
         }
-        
+
         return stack.pop();
     }
 }
